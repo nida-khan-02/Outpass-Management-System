36ff2d1d30ce81f4a335b7f27212a60892236051
nida-khan-02 <nida.02k@gmail.com>
2024-10-02 18:36:39 +0530

    Update authService.js

--- my-outpass/src/Services/authService.js
+++ my-outpass/src/Services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://outpass-management-system-backend.vercel.app/api/auth';
 export const login = async (credentials) => {
   console.log('API url:', API_URL);
 
